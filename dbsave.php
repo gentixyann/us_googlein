1511add6774f3ffcff28b32cba17722bfa6bc53d
Gento <a551.return@gmail.com>
2018-02-15 15:25:16 +0800

    重複させん

--- dbsave.php
+++ dbsave.php
@@ -12,8 +12,29 @@ if(isset($_GET) && !empty($_GET["userID"]) && !empty($_GET["userName"]) && !empt
    $nick_name = $_GET['userName'];
    $email = $_GET['userEmail'];
    $password = $_GET['userID'];
- 
+    
      try {
+         $sql = "SELECT COUNT(*) as `cnt` FROM `whereis_members` WHERE `email`=?";
+         
+         //$sql = "SELECT COUNT(*) as `cnt` FROM `whereis_members` WHERE `email`=?";
+         
+         //sql文実行
+ $data = array($_GET["userEmail"]);
+         
+ $stmt = $dbh->prepare($sql);
+ $stmt->execute($data);
+
+ //件数とる
+ $count = $stmt->fetch(PDO::FETCH_ASSOC);
+         
+         
+         if ($count['cnt'] > 0) {
+  //重複エラー
+           header('Location: json_map.html');
+           exit();
+ }else{
+         
+         
   // DBに会員情報を登録するSQL文を作成
   // now() MYSQLが用意している関数。現在日時を取得できる
         $sql = "INSERT INTO `whereis_members` (`nick_name`, `email`, `password`, `created`, `modified`) VALUES (?,?,?,now(),now()) ";
@@ -31,7 +52,7 @@ if(isset($_GET) && !empty($_GET["userID"]) && !empty($_GET["userName"]) && !empt
   // ログインページへ遷移
        header('Location: json_map.html');
         exit();
-
+         }
 
       } catch (Exception $e) {
         // tryで囲まれた処理でエラーが発生したときにやりたい処理を記述
