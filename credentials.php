68889afbace533b0166a1c8b97f48e99481af374
Gento <a551.return@gmail.com>
2018-03-19 18:52:31 +0900

    sendgridでメール成功

---
+++ credentials.php
@@ -0,0 +1,4 @@
+<?php
+
+$API_KEY = "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";
+?>
---
+++ sendgrid.php
@@ -0,0 +1,31 @@
+<?php
+	// You need to install the sendgrid client library so run: composer require sendgrid/sendgrid
+	require 'vendor/autoload.php';
+	// contains a variable called: $API_KEY that is the API Key.
+	// You need this API_KEY created on the Sendgrid website.
+	include_once('credentials.php');
+	
+	$FROM_EMAIL = 'wheview@gmail.com';
+	// they dont like when it comes from @gmail, prefers business emails
+	$TO_EMAIL = 'wheview@gmail.com';
+	// Try to be nice. Take a look at the anti spam laws. In most cases, you must
+	// have an unsubscribe. You also cannot be misleading.
+	$subject = "my first test";
+	$from = new SendGrid\Email(null, $FROM_EMAIL);
+	$to = new SendGrid\Email(null, $TO_EMAIL);
+	$htmlContent = '<p>Hello</p>';
+	// Create Sendgrid content
+	$content = new SendGrid\Content("text/html",$htmlContent);
+	// Create a mail object
+	$mail = new SendGrid\Mail($from, $subject, $to, $content);
+	
+	$sg = new \SendGrid($API_KEY);
+	$response = $sg->client->mail()->send()->post($mail);
+			
+	if ($response->statusCode() == 202) {
+		// Successfully sent
+		echo 'done';
+	} else {
+		echo 'false';
+	}
+?>
