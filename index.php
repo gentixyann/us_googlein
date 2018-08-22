47471f41704995a3e9737bfa7ce494b515450d8d
Gento <a551.return@gmail.com>
2018-08-22 21:45:23 +0900

    map option 追加

--- index.php
+++ index.php
@@ -1,153 +1,302 @@
 <?php
 session_start();
+require('dbconnect.php');
 
+try{
+ //markerしてる人の情報とる
+    $sql = "SELECT * FROM `whereis_map` ";
 
-// DBに接続
-  require('dbconnect.php');
+    //sql実行
+    //実行待ち
+    $stmt = $dbh->prepare($sql);
+    //実行
+    $stmt->execute();
 
-$lang = "en";
+    $marker_info["Markers"] = array();
+     while (1) {
 
-if (isset($_GET["lang"])){
-  $_SESSION["lang"] = $_GET["lang"];
-  $lang = $_SESSION["lang"];
-}else{
-    $_SESSION["lang"] =  $lang;
-}
+          //PDOはPHP Data Objects FETCH_ASSOCは連想配列で取り出す意味
+     $marker_data = $stmt->fetch(PDO::FETCH_ASSOC);
+         $marker_info["Markers"][] = $marker_data;
 
+         if ($marker_data == false){
+         break;//中断する
+     }else{
+    $json = json_encode($marker_info, JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+        //var_dump($json);
+   }
+     }
 
-function trans($word,$lang){
-  //翻訳ファイルを読み込み
-  require("lang/words_".$lang.".php");
+}catch(Exception $e){
 
-  //配列からデータを取得
-  $trans_word = $word_list[$word];
-
-  //文字を返す
-  return $trans_word;
-}
+  }
 
 ?>
 
 
-
-
-<html>
+<!doctype html>
+<html lang="ja">
 <head>
-    <meta charset="UTF-8">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Where is *(アスタリスク)</title>
+    <meta charset="utf-8" />
+    <title>See</title>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <meta name="Nova theme" content="width=device-width, initial-scale=1">
-
-    <!--    Goodle クライアントID-->
-    <meta name="google-signin-client_id" content="1028844914150-vequeee5hlji30ij1ci4v8ebdva5o42v.apps.googleusercontent.com">
-
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="css/responsive.css" />
-    <link rel="stylesheet" href="css/login.css" />
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/hero.css" />
+    <link rel="stylesheet" type="text/css" href="css/map_style.css">
+    <link rel="stylesheet" href="css/searchAddress.css" />
+     <link rel="stylesheet" href="css/modal.css" />
 
-    <!--    Goodleのアカウント使用で必要-->
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
-
-<style>
-    .g-signin2 {
-        width: 100%;
-    }
-
-    .g-signin2>div {
-        margin: 0 auto;
-    }
-</style>
-
-
-
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA0jIuanGD4d4KNxkq2w4jbwxbQ0tMImXc&libraries=places"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 </head>
 
+  <!--    modal-->
+<div class="modal fade" id="Modal01" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="wrapper">
+ <iframe class="modalContent" src="https://www.google.com/maps/embed?pb=!4v1532784093637!6m8!1m7!1sCAoSLEFGMVFpcE1rbGd3anBaVnB3bkZUZjJSVERQZXMtMnpiVmxESGdWZkctY2Mx!2m2!1d37.32681888304!2d140.5839723084!3f253.66865168581887!4f-0.7804159282870557!5f0.7820865974627469" frameborder="0" style="border:0" allowfullscreen></iframe>
+        </div>
+    </div>
+</div>
+  <!--    end of sv modal-->
 
 <body>
-    <!-- Navigation
-    ================================================== -->
-<div class="hero-background">
-    <img class="strips" src="earth.png">
+  <div class="fullheight">
+        <input id="pac-input" class="controls" type="text" placeholder="Search">
 
-    <div class="container">
-<div class="header-container header">
-
-</div>
-<!--end of header-->
-
-        <div class="hero row">
-<div class="hero-right col-sm-6 col-sm-6">
-    <h1 class="header-headline bold">
-        <?php echo trans("世界の景色をお手軽に",$lang); ?>
-        <br>
-    </h1>
+<div class="container">
+    <img data-toggle="modal" data-target="#Modal01" src="img/btn_03.png" width="52" height="15" alt="Japanese">
+    <img src="img/btn_04.png" width="52" height="15" alt="English">
 </div>
 
-            <form method="POST" action="">
-                <div class="col-sm-6 col-sm-6 ">
-                    <div class="loginpanel">
 
-                        <a href="json_map.php" class="submit_button">
-                  <input type="button" value="Visitor" class="submit_button">
-                  </a>
+ <div id="gmap_wrapper">
+  <div id="map_canvas"></div>
+    </div>
 
-                        <div id="forget_pw">
-                            <p>投稿にはログインが必要だぜ</p>
-                        </div>
+</div>
 
-                        <div class="hr">
-                            <div></div>
-                            <div>OR</div>
-                            <div></div>
-                        </div>
 
-                        <div id="smaho">
-                        <div class="social">
 
-                            <div class="g-signin2" data-onsuccess="onSignIn"
-                                 data-width=240 data-height="50"  data-longtitle="true"></div>
 
-                        </div>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        </div><!--end of hero row-->
-    </div><!--end of container-->
-</div><!--end of hero-background-->
 
-    <script src="https://www.gstatic.com/firebasejs/4.9.1/firebase.js"></script>
+<script type="text/javascript">
 
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDVezH32ZycwFc8mHGYyhQgK0ovBgX1WGY",
-    //apiKey: "AIzaSyDStsWYUik9kLI-hbkIPQxSsBX-X-smIlw",
-    authDomain: "where-map-e3a10.firebaseapp.com",
-    databaseURL: "https://where-map-e3a10.firebaseio.com",
-    projectId: "where-map-e3a10",
-    storageBucket: "where-map-e3a10.appspot.com",
-    messagingSenderId: "1028844914150"
+    $(function(){
+    var json = <?php echo $json ?>;
+    console.log(json);
+
+    var data=jsonRequest(json);
+    console.log(data);
+    initialize(data);
+    });
+
+   // JSONファイル読み込み完了
+function jsonRequest(json){
+  var data=[];
+
+    //Markersはjsonデータ配列のMarkerのこと。配列の塊を全て読み込んで、その数を変数nにする。
+  if(json.Markers){
+    var n=json.Markers.length;
+    for(var i=0;i<n;i++){
+      data.push(json.Markers[i]);
+    }
+  }
+  return data;
+}
+
+
+
+//    console = null; // warningを表示しないようnullで(ry
+  var currentInfoWindow = null;
+
+  function createClickCallback(marker, infoWindow) {
+    return function() {
+      if (currentInfoWindow){
+        currentInfoWindow.close();
+      }
+      infoWindow.open(marker.getMap(), marker);
+
+      currentInfoWindow = infoWindow;
+    };
+  }
+
+    var map;
+    var marker = "";
+    var randomLat = Math.random()*140 - 70;
+    var randomLng = Math.random()*360 - 180;
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+
+// マップを生成して、複数のマーカーを追加
+function initialize(data/*Array*/){
+
+//この変数がmapのoption
+  var op={
+    zoom:8,
+    //center:new google.maps.LatLng(34.67347038699344,135.44394850730896),
+     center:new google.maps.LatLng(randomLat.toFixed(6),randomLng.toFixed(6)),
+     gestureHandling: 'greedy',
+     mapTypeControl: false,
+     panControl: false,
+     zoomControl: false,
+     streetViewControl: false,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
   };
-  firebase.initializeApp(config);
+
+//基本となるマップのobject
+   map=new google.maps.Map(document.getElementById("map_canvas"),op);
+
+  var i=data.length;
+    while(i-- >0){
+        var dat = data[i];
+        var marker=new google.maps.Marker({
+            position:new google.maps.LatLng(dat.lat,dat.lng),
+            map:map
+        });
+        var infoWindow = new google.maps.InfoWindow({
+            content:'<div class="infoWindow">'+
+            //dat.movie_infoはDBのカラム名
+             '<p>'+dat.movie_info+'</p>'+
+             '</div>'
+        });
+
+         google.maps.event.addListener(marker, 'click', createClickCallback(marker, infoWindow));
+    }
+
+     var geocoder = new google.maps.Geocoder();
+            //createElementでdivを生成。
+     var geolocationDiv = document.createElement('div');
+            //GeolocationControlで現在地とる。jsのAPI
+     var geolocationControl = new GeolocationControl(geolocationDiv, map);
+
+       //現在地ボタンをmapの中に表示
+      map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push
+      (geolocationDiv);
+
+      //住所検索のinputをmapの中に表示
+      map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+      map.addListener('bounds_changed', function(){
+      searchAddress()
+   })
+}//end of initialize()
+
+//住所検索の関数
+function searchAddress(){
+        searchBox.setBounds(map.getBounds());
+        var markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+
+          // Clear out the old markers.
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
+
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              //console.log("Returned place contains no geometry");
+              alert("Returned place contains no geometry");
+              return;
+            }
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              title: place.name,
+              //zoom: 15,
+              position: place.geometry.location
+
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
+          map.setZoom(15);
+        });
+}
+
+  function GeolocationControl(controlDiv, map) {
+
+    // Set CSS for the control button
+    //createElementでdivを作る。その変数がcontrolUI。ボタンの箱作ってる
+    var controlUI = document.createElement('div');
+    controlUI.style.backgroundColor = '#fff';
+	controlUI.style.border = 'none';
+	controlUI.style.outline = 'none';
+	controlUI.style.width = '28px';
+	controlUI.style.height = '28px';
+	controlUI.style.borderRadius = '2px';
+	controlUI.style.boxShadow = '0 1px 4px rgba(0,0,0,0.3)';
+	controlUI.style.cursor = 'pointer';
+	controlUI.style.marginRight = '10px';
+	controlUI.style.padding = '0px';
+	controlUI.title = 'Your Location';
+	controlDiv.appendChild(controlUI);
+
+    // Set CSS for the control text
+    //ボタン作ってる
+    var controlText = document.createElement('div');
+    controlText.style.margin = '2px';
+	controlText.style.width = '28px';
+    controlText.style.height = '28px';
+    controlText.style.backgroundImage = 'url(img/gps10.png)';
+    controlText.style.backgroundSize = '17px 17px';
+	controlText.style.backgroundPosition = '4px 4px';
+	controlText.style.backgroundRepeat = 'no-repeat';
+	controlText.id = 'you_location_img';
+	controlUI.appendChild(controlText);
+
+    // Setup the click event listeners to geolocate user
+    //controlUIクリックしたらgeolocate関数発動
+    google.maps.event.addDomListener(controlUI, 'click', geolocate);
+}
+
+    //現在地ボタン押した時のgeolocate
+    function geolocate() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+            // Create a marker and center map on user location
+             marker = new google.maps.Marker({
+                position: pos,
+                draggable: true,
+                animation: google.maps.Animation.DROP,
+                //zoom: 12,
+                map: map
+            });
+            //座標をセット。１番目の引数には設定する中心座標
+            map.setCenter(pos);
+            map.setZoom(15);
+        });
+    }else {
+               //Geolocation API使えん
+         handleLocationError(false, map.getCenter());
+            }
+}
+
 </script>
 
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-
-    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
-    <script type="text/javascript" src="js/google_login.js"></script>
-  <!--   <script src="js/login.js"></script> -->
-    <!-- <script src="js/script.js"></script> -->
-
+ <!-- <script src="js/map.js"></script> -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 </body>
-
 </html>
---
+++ login_google.php
@@ -0,0 +1,153 @@
+<?php
+session_start();
+
+
+// DBに接続
+  require('dbconnect.php');
+
+$lang = "en";
+
+if (isset($_GET["lang"])){
+  $_SESSION["lang"] = $_GET["lang"];
+  $lang = $_SESSION["lang"];
+}else{
+    $_SESSION["lang"] =  $lang;
+}
+
+
+function trans($word,$lang){
+  //翻訳ファイルを読み込み
+  require("lang/words_".$lang.".php");
+
+  //配列からデータを取得
+  $trans_word = $word_list[$word];
+
+  //文字を返す
+  return $trans_word;
+}
+
+?>
+
+
+
+
+<html>
+<head>
+    <meta charset="UTF-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Where is *(アスタリスク)</title>
+    <meta name="Nova theme" content="width=device-width, initial-scale=1">
+
+    <!--    Goodle クライアントID-->
+    <meta name="google-signin-client_id" content="1028844914150-vequeee5hlji30ij1ci4v8ebdva5o42v.apps.googleusercontent.com">
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/responsive.css" />
+    <link rel="stylesheet" href="css/login.css" />
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <!--    Goodleのアカウント使用で必要-->
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+
+<style>
+    .g-signin2 {
+        width: 100%;
+    }
+
+    .g-signin2>div {
+        margin: 0 auto;
+    }
+</style>
+
+
+
+</head>
+
+
+<body>
+    <!-- Navigation
+    ================================================== -->
+<div class="hero-background">
+    <img class="strips" src="earth.png">
+
+    <div class="container">
+<div class="header-container header">
+
+</div>
+<!--end of header-->
+
+        <div class="hero row">
+<div class="hero-right col-sm-6 col-sm-6">
+    <h1 class="header-headline bold">
+        <?php echo trans("世界の景色をお手軽に",$lang); ?>
+        <br>
+    </h1>
+</div>
+
+            <form method="POST" action="">
+                <div class="col-sm-6 col-sm-6 ">
+                    <div class="loginpanel">
+
+                        <a href="json_map.php" class="submit_button">
+                  <input type="button" value="Visitor" class="submit_button">
+                  </a>
+
+                        <div id="forget_pw">
+                            <p>投稿にはログインが必要だぜ</p>
+                        </div>
+
+                        <div class="hr">
+                            <div></div>
+                            <div>OR</div>
+                            <div></div>
+                        </div>
+
+                        <div id="smaho">
+                        <div class="social">
+
+                            <div class="g-signin2" data-onsuccess="onSignIn"
+                                 data-width=240 data-height="50"  data-longtitle="true"></div>
+
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div><!--end of hero row-->
+    </div><!--end of container-->
+</div><!--end of hero-background-->
+
+    <script src="https://www.gstatic.com/firebasejs/4.9.1/firebase.js"></script>
+
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDVezH32ZycwFc8mHGYyhQgK0ovBgX1WGY",
+    //apiKey: "AIzaSyDStsWYUik9kLI-hbkIPQxSsBX-X-smIlw",
+    authDomain: "where-map-e3a10.firebaseapp.com",
+    databaseURL: "https://where-map-e3a10.firebaseio.com",
+    projectId: "where-map-e3a10",
+    storageBucket: "where-map-e3a10.appspot.com",
+    messagingSenderId: "1028844914150"
+  };
+  firebase.initializeApp(config);
+</script>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script type="text/javascript" src="js/google_login.js"></script>
+  <!--   <script src="js/login.js"></script> -->
+    <!-- <script src="js/script.js"></script> -->
+
+</body>
+
+</html>
